b70d75dd59609b4ef2ac2882829df95293315366
Martin Halaj <m.halaj@websystem.sk>
2021-02-01 09:24:53 +0100

    add clr btn to bottom & filter icon

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -5,9 +5,14 @@
                 <section class="w-full max-w-6xl mx-auto">
                     <div class="grid md:grid-cols-12 gap-5 p-2">
                         <aside class="md:col-span-3 md:pt-0 p-2 shadow-xl justifi-center">
-                            <svg class="w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
-                              </svg>
+                            <div class="w-full bg-blue-900 text-white p-4 shadow-xl rounded">
+                                <svg class="w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+                                </svg>
+                            </div>
+
                             @include('cars.partials._filters')
                         </aside>
                         <div class="md:col-span-9 p-4">
@@ -15,10 +20,10 @@
 
                                 @if ($cars->count())
 
-                                  @each('cars.partials._content', $cars, 'car')
-                                  <div class="p-2">
-                                      {{ $cars->appends(request()->query())->links() }}
-                                  </div>
+                                    @each('cars.partials._content', $cars, 'car')
+                                    <div class="p-2">
+                                        {{ $cars->appends(request()->query())->links() }}
+                                    </div>
 
                                 @else
 
--- resources/views/cars/partials/_filters.blade.php
+++ resources/views/cars/partials/_filters.blade.php
@@ -1,16 +1,15 @@
 
-<div class="w-full bg-blue-900 text-white p-4 rounded shadow">
-    <p>
-        <a href="{{ route('cars.index') }}">Clear all filters</a>
-    </p>
-</div>
-
 @foreach($mappings as $key => $map)
 
     @include('cars.partials._filter_list', compact('key', 'map'))
 
 @endforeach
 
+<div class="w-1/2 bg-blue-900 text-white p-2 rounded shadow">
+    <p>
+        <a href="{{ route('cars.index') }}">Clear all filters</a>
+    </p>
+</div>
 
 
 
